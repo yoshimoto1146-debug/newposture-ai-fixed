3f7a0b28b98b63b4c50dd84222776152ff8375ed
yoshimoto1146-debug <yoshimoto1146@gmail.com>
2026-01-31 12:39:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
   
   return {
     plugins: [react()],
     define: {
-      // 引用符の扱いを厳密にしてビルドエラーを防ぐ
       'process.env.API_KEY': JSON.stringify(env.API_KEY || process.env.API_KEY || ''),
       'process.env.STORE_NAME': JSON.stringify(env.STORE_NAME || process.env.STORE_NAME || '')
     },
